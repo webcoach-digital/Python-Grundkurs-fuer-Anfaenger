5a85aee31e44d9c1de41894fc1ca9e2ecc8d9de7
petereneric <ericschumacher@outlook.de>
2023-09-18 16:05:34 +0200

    impact

--- Variablen & Datentypen/a) Variablen & Datentypen.py
+++ Variablen & Datentypen/a) Variablen & Datentypen.py
@@ -1,1 +1,24 @@
-print("test")
+# Variablen
+my_foot_size = 45
+my_promille = 0.0
+my_name = "Eric"
+me_female = False
+
+# Datentyp: integer
+# => Ganze Zahlen
+your_foot_size = 46
+print(type(your_foot_size))
+
+# Datentyp: float
+# => Dezimal Zahlen
+your_promille = 0.0
+print(type(your_promille))
+
+# Datentyp: string
+your_name = "Tyler"
+your_last_name = 'Durden'
+print(type(your_name))
+
+# Datentyp: boolean
+you_male = False
+print(type(you_male))
